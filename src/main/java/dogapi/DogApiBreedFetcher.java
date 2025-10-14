f5ab69ce1f579d4b51c10c1256ba241f93ab29b7
0NCEbot <danbhnguyen2006@gmail.com>
2025-10-13 21:27:14 -0400

    Removed unnecessary exception handling

--- src/main/java/dogapi/DogApiBreedFetcher.java
+++ src/main/java/dogapi/DogApiBreedFetcher.java
@@ -4,7 +4,6 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
@@ -52,7 +51,7 @@ public class DogApiBreedFetcher implements BreedFetcher {
                 throw new BreedNotFoundException("Breed: " + breed + " does not exist");
             }
         }
-        catch (IOException | JSONException event) {
+        catch (IOException event) {
             throw new BreedNotFoundException("Breed: " + breed + " does not exist");
         }
     }
